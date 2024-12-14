05fa08b2f335fb6b2b0da21e5e2190188da4a99a
FelipeTanusR <fetanus@usp.br>
2024-12-13 22:28:15 -0300

    criando elementos ui

--- app-fazenda/src/components/add-json-dialog.tsx
+++ app-fazenda/src/components/add-json-dialog.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 import {
     Dialog,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -11,14 +14,27 @@ import {
 import { Button } from './ui/button'
 import { Label } from '@radix-ui/react-label'
 import { Input } from './ui/input'
+import { DialogClose } from '@radix-ui/react-dialog'
 
 export default function AddJsonDialog() {
+
+    const [brinco, setBrinco] = useState("");
+    const [d_nasc, setD_nasc] = useState("");
+
+
+    const handleSave = ()=>{
+        console.log({
+            brinco,
+            d_nasc
+        });
+    };
+
   return (
     <Dialog>
         <DialogTrigger asChild>
             <Button>Adicionar animal</Button>
         </DialogTrigger>    
-        <DialogContent>
+        <DialogContent className='max-w-4xl'>
             <DialogHeader>
             <DialogTitle>Cadastro Animal</DialogTitle>
             <DialogDescription>Registre um animal novo.</DialogDescription>
@@ -27,8 +43,27 @@ export default function AddJsonDialog() {
             <div className='grid gap-4'>
                 <div className='grid gap-2'>
                     <Label>Brinco</Label>
-                    <Input placeholder='25001' />
+                    <Input value={brinco}placeholder='25001' 
+                    onChange={(e) => setBrinco(e.target.value) }>
+                    </Input>
+
                 </div>
+                <div className='grid gap-2'>
+                    <Label>Data Nascimento</Label>
+                    <Input value={d_nasc} placeholder='dd/mm/aaaa'
+                    onChange={(e)=> setD_nasc(e.target.value)} />
+                </div>
+
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Fechar
+                        </Button>
+                    </DialogClose>
+                    <Button disabled={!brinco||!d_nasc} onClick={handleSave}>Salvar</Button>
+                </DialogFooter>
+
+
             </div>
 
 
